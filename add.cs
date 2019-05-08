54aeee6e5f4ef846448631588ed2b7be170326ad
IRIS NG <49146877+NgHuiLingIris@users.noreply.github.com>
2019-05-08 19:07:39 +0800

    Add files via upload

---
+++ add.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CA_ShoppingCart.Util
+{
+    public class Add
+    {
+        public static string getCode()
+        {
+            string activationCode = Guid.NewGuid().ToString();
+            return activationCode;
+        }
+    }
+}
